bfe0b432b3ef58a8a07dfc9db22543f9ad4cb4a2
shubhankarnikam-45 <shubhankarnikam45@gmail.com>
2023-09-01 15:59:45 +0530

    created

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 // your code here
+
+
+let btn = document.getElementById("button");
+
+
+
+let name = document.getElementById("name");
+
+let year = document.getElementById("year");
+
+btn.addEventListener("click", (e) => {
+
+    e.preventDefault();
+
+    let h1TagData = document.getElementById("url");
+
+
+    if (name.value != "")
+        h1TagData.innerText += `?name=${name.value}`;
+
+    if (year.value != "")
+        h1TagData.innerText += `&year=${year.value}`;
+})
+
+
+
+
